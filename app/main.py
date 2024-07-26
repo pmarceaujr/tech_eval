1ed4e8d5ac87d922ba2305bf6dd01d7b1ac50c20
pmarceaujr <paul@themarceaus.net>
2024-07-26 18:26:56 -0400

    Your branch is up to date with 'origin/main'.
     Changes to be committed:
    	modified:   app/__pycache__/main.cpython-312.pyc
    	modified:   app/main.py

--- app/main.py
+++ app/main.py
@@ -1,7 +1,9 @@
 from fastapi import Depends, FastAPI, status, Response
 from sqlalchemy.orm import Session
+from sqlalchemy import or_
 from .database import get_db, engine
 from app import models, schemas
+from datetime import date
 
 app = FastAPI()
 
@@ -13,20 +15,20 @@ def welcome():
     return {"message": "Welcome the music project, the server and API are running fine."}
 
 @app.get("/artists/", response_model=list[schemas.Artist])
-def list_artists(db: Session = Depends(get_db), limit: int = 10):
-    artists = db.query(models.Artist).limit(limit).all()
+def list_artists(db: Session = Depends(get_db), limit: int = 10, offset: int = 0):
+    artists = db.query(models.Artist).limit(limit).offset(offset).all()
     return artists
 
 @app.get("/artists_albums/", response_model=list[schemas.ArtistAlbum])
-def list_artists_with_albums(db: Session = Depends(get_db), limit: int = 10):
-    artists = db.query(models.Artist).limit(limit).all()
+def list_artists_with_albums(db: Session = Depends(get_db), limit: int = 10, offset: int = 0):
+    artists = db.query(models.Artist).limit(limit).offset(offset).all()
     return artists
 
 
 
 @app.get("/albums/", response_model=list[schemas.AlbumBase])
-def list_albums(db: Session = Depends(get_db), limit: int = 10):
-    albums = db.query(models.Album).limit(limit).all()
+def list_albums(db: Session = Depends(get_db), limit: int = 10, offset: int = 0):
+    albums = db.query(models.Album).limit(limit).offset(offset).all()
     return albums
 
 
@@ -48,8 +50,20 @@ def create_album(artist_id: int, payload: schemas.AlbumCreate, db: Session = Dep
 
 
 @app.get("/albums/{artist_id}", response_model=list[schemas.Album])
-def get_artist_albums(artist_id: int,  db: Session = Depends(get_db)):
+def get_artist_albums(artist_id: int,  db: Session = Depends(get_db), limit: int = 10, offset: int = 0, release_date: date = None, price: int = None):
     # items = crud.get_itemss_by_user(user_id, db)
-    albums = db.query(models.Album).filter(models.Album.artist_id == artist_id).all()
+    filters = []
+    if release_date:
+        print("Date")
+        filters.append(models.Album.release_date == release_date)
+    if price:
+        print("Price)")
+        filters.append(models.Album.price == price))
+
+    albums = db.query(models.Album).\
+        filter(models.Album.artist_id == artist_id).\
+        filter(
+            or_(filters)).\
+        limit(limit).offset(offset).all()
     return albums
 
