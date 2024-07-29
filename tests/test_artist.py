eda1a00e788218e1d8cf47bca14fbf7312e08ed8
pmarceaujr <paul@themarceaus.net>
2024-07-29 17:25:17 -0400

    REfactor code for better error handling and more test coverage  On branch dev/PLM/feature/refactor_tests  Changes to be committed: 	modified:   app/__pycache__/main.cpython-312.pyc 	modified:   app/__pycache__/models.cpython-312.pyc 	modified:   app/main.py 	modified:   app/models.py 	modified:   bay_music.db 	modified:   bay_music_test.db 	modified:   tests/__pycache__/test_artist.cpython-312-pytest-8.3.2.pyc 	modified:   tests/test_artist.py

--- tests/test_artist.py
+++ tests/test_artist.py
@@ -1,6 +1,7 @@
 import pytest
 from fastapi import FastAPI
 from fastapi.testclient import TestClient
+from sqlalchemy.exc import IntegrityError
 from app.main import app
 from app import schemas
 from sqlalchemy import create_engine
@@ -51,23 +52,35 @@ def test_artists(session):
 
 def test_welcome(client):
     res = client.get("/welcome")
-    assert res.json().get('message') == 'Welcome the music project, the server and API are running fine.'
     assert res.status_code == 200
 
-
-@pytest.mark.parametrize("artist_name, expected",[
-    ("Motley Crue", "Motley Crue" ),
-    ("Luke Combs", "Luke Combs"),
-    ("Van Halen", "Van Halen"),
-    ("Steve Miller Band", "Steve Miller Band"),
-    ("Dave Matthews Band", "Dave Matthews Band"),
-    ("Lee Brice", "Lee Brice")
+@pytest.mark.parametrize("artist_name, genre, expected, expected_response",[
+    ("Motley Crue", "Rock", "Motley Crue", 201 ),
+    ("Motley Crue", "Rock", "Motley Crue", 409 ),
+    ("Luke Combs", "Country", "Luke Combs", 201 ),
+    ("Van Halen",  "Rock", "Van Halen", 201 ),
+    ("Steve Miller Band", "Rock", "Steve Miller Band", 201 ),
+    ("Dave Matthews Band",  "Rock", "Dave Matthews Band", 201 ),
+    ("Lee Brice", "Country", "Lee Brice", 201 )
 ])
-def test_create_artist(client, artist_name, expected):
-    res = client.post("/artist", json={"name": artist_name})
-    new_artist = schemas.Artist(**res.json())
-    assert new_artist.name == expected
-    assert res.status_code == 201 
+def test_endpoints_not_founf(client, artist_name, genre, expected, expected_response):
+    res = client.post("/artist", json={"name": artist_name, "genre": genre})
+    assert res.status_code == expected_response 
+
+
+@pytest.mark.parametrize("artist_name, genre, expected, expected_response",[
+    ("Motley Crue", "Rock", "Motley Crue", 201 ),
+    ("Motley Crue", "Rock", "Motley Crue", 409 ),
+    ("Luke Combs", "Country", "Luke Combs", 201 ),
+    ("Van Halen",  "Rock", "Van Halen", 201 ),
+    ("Steve Miller Band", "Rock", "Steve Miller Band", 201 ),
+    ("Dave Matthews Band",  "Rock", "Dave Matthews Band", 201 ),
+    ("Lee Brice", "Country", "Lee Brice", 201 )
+])
+def test_create_artist(client, artist_name, genre, expected, expected_response):
+    res = client.post("/artist", json={"name": artist_name, "genre": genre})
+    assert res.status_code == expected_response 
+
 
 @pytest.mark.parametrize("album_name, release_date, price, artist_id, expected, status_code",[
     ("Too Fast for Love", "1981-11-10", 12.99, 1, "Too Fast for Love", 201 ),
@@ -77,7 +90,7 @@ def test_create_artist(client, artist_name, expected):
     ("What You See Is What You Get", "2019-11-08", 19.99, 2, "What You See Is What You Get", 201 ),
     ("Growin' Up", "2022-06-24", 12.99, 2, "Growin' Up", 201 ),
     # Error entry
-    ("Growin' Up123", "2022-00-24", 12.99, 2, "Growin Up", 201 ),
+    ("Growin' Up123", "2022-00-24", 12.99, 2, "Growin Up", 422 ),
     ("Gettin Old", "2023-03-24", 12.99, 2, "Gettin Old", 201 ),
     ("Van Halen", "1978-02-10", 9.99, 3, "Van Halen", 201 ),
     ("Van Halen II", "1979-03-23", 8.99, 3, "Van Halen II", 201 ),
@@ -92,8 +105,9 @@ def test_create_artist(client, artist_name, expected):
     ("Before These Crowded Streets", "1998-01-01", 14.99, 5, "Before These Crowded Streets", 201 ),
     ("Stand Up", "2005-01-01", 14.99, 5, "Stand Up", 201 ),
     # Error entry
-    ("Stand Up123", "005-01-01", 14.99, 5, "Stand Up", 201 ),
-    ("Love Like Crazy", "2010-01-01", 13.99, 6, "Love Like Crazy", 201 ),
+    ("Stand Up123", "005-01-01", 14.99, 5, "Stand Up", 422 ),
+
+    ("Love Like Crazy", "2010-01-01", 13.99, 612312, "Love Like Crazy", 201 ),
     ("Hard 2 Love", "2012-01-01", 13.99, 6, "Hard 2 Love", 201 ),
     ("Lee Brice", "2017-01-01", 15.99, 6, "Lee Brice", 201 ),
     ("Hey World", "2020-01-01", 14.99, 6, "Hey World", 201 )
@@ -101,6 +115,4 @@ def test_create_artist(client, artist_name, expected):
 ])
 def test_create_album(client, album_name, release_date, price, artist_id, expected, status_code):
     res = client.post(f"/artist/{artist_id}/album", json={"name": album_name, "release_date": release_date, "price": price, "artist_id": artist_id})
-    new_album = schemas.Album(**res.json())
-    # assert new_album.name == expected
     assert res.status_code == status_code     
